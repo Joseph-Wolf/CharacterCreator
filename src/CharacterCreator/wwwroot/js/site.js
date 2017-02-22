24256cd1ce5b0581922ae0563bb6232fe4f82a5e
Wolf, Joseph <jbone@metlife.com>
2017-02-22 10:54:13 -0500

    Reworking some CSS and JS. using localStorage provided by jQuery and simplifying the tab session storage mechanism.

--- src/CharacterCreator/wwwroot/js/site.js
+++ src/CharacterCreator/wwwroot/js/site.js
@@ -1,22 +1,23 @@
 ﻿$(document).ready(function () {
-    'use strict';
+    "use strict";
     var editMode = false;
     var commandKey = 189;
     var editModeKey = 69;
     var keyMap = {commandKey: false, editModeKey: false};
 
-    $('.jqTabs').tabs({ 
-        activate: function (event, ui) {
-            window.sessionStorage.setItem($(this).prop('id'), ui.newTab.parent().children().index(ui.newTab));
-        }
-    }).each(function setJQTabsActiveTab(){
-        $(this).tabs({ active: parseInt(window.sessionStorage.getItem($(this).prop('id'))) || 0 });
-    }); 
+    //-----------------START Process Tabs--------------------------------
+    $(".jq-tabs").tabs({ //Add some settings for the tabs
+        activate: function (event, ui) { //When a tab is clicked
+            localStorage["active-tab"] = ui.newTab.index(); //Store the tab index
+        },
+        active: parseInt(localStorage["active-tab"]) || 0 //set the active tab to either a stored value or to 0
+    });
+    //-------------------END Process Tabs--------------------------------
 
-    $('img.galleryImage').click(function displayLargerImage() {
-        $('#GalleryCenterImage').html($('<img/>', {
-            src: $(this).prop('src'),
-            class: 'autoResize'
+    $("img.galleryImage").click(function displayLargerImage() {
+        $(".gallery-center-image").html($("<img/>", {
+            src: $(this).prop("src"),
+            class: "autoResize"
         }));
     });
 
@@ -32,20 +33,20 @@
                 if (editMode) {
                     //TODO: reset css to default?
 
-                    $('.jQResizable').resizable({ //Set resizable panels
+                    $(".jQResizable").resizable({ //Set resizable panels
                         stop: function (event, ui) { //Send coordinates to controller on stop
-                            var formId = '#SaveUIForm';
-                            $(formId).find('#css').val('#' + $(ui.element).prop('id') + '{max-width:' + ui.size.width + 'px;width:' + ui.size.width + 'px;max-height:' + ui.size.height + 'px;height:' + ui.size.height + 'px;}')
+                            var formId = "#SaveUIForm";
+                            $(formId).find("#css").val("#" + $(ui.element).prop("id") + "{max-width:" + ui.size.width + "px;width:" + ui.size.width + "px;max-height:" + ui.size.height + "px;height:" + ui.size.height + "px;}");
                             $.ajax({
-                                url: $(formId).attr('action'),
-                                method: $(formId).attr('method'),
+                                url: $(formId).attr("action"),
+                                method: $(formId).attr("method"),
                                 data: $(formId).serialize()
                             });
                         }
                     });
                 } else {
                     //TODO: apply changes to all characters or just one?
-                    $('.jQResizable').resizable('destroy'); //Destroy resizable if it is set up
+                    $(".jQResizable").resizable("destroy"); //Destroy resizable if it is set up
                 }
             }
         }
@@ -53,16 +54,16 @@
         keyMap[e.keyCode] = false;
     });
 
-    $('.UploadImageInput').change(function submitImageForm() { //submit image as soon as an image is selected
-        var tmpForm = $(this).closest('form').clone();
-        $(tmpForm).find('.UploadImageHidden').removeClass('UploadImageHidden');
-        $(tmpForm).find('.UploadImageInput').hide();
+    $(".UploadImageInput").change(function submitImageForm() { //submit image as soon as an image is selected
+        var tmpForm = $(this).closest("form").clone();
+        $(tmpForm).find(".UploadImageHidden").removeClass("UploadImageHidden");
+        $(tmpForm).find(".UploadImageInput").hide();
         //display image preview when loaded
         if (this.files && this.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
-                $('img.UploadImagePreview').attr('src', e.target.result);
-            }
+                $("img.UploadImagePreview").attr("src", e.target.result);
+            };
             reader.readAsDataURL(this.files[0]);
         }
         $(tmpForm).dialog();
