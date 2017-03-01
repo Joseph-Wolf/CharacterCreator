56e0dc3775a5a479225f3e907a492324c0183a5e
Wolf, Joseph <jbone@metlife.com>
2017-03-01 13:55:01 -0500

    Implementing a mock in memory database for tests. Implemented the index tests

--- test/CharacterCreator.Tests/Controllers/HomeControllerTests.cs
+++ test/CharacterCreator.Tests/Controllers/HomeControllerTests.cs
@@ -1,18 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CharacterCreator.Controllers;
+using CharacterCreator.Models;
+using CharacterCreator.Services;
+using CharacterCreator.Tests.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace CharacterCreator.Tests.Controllers
 {
-    public class HomeControllerTests
+    public class HomeControllerTests : IDisposable
     {
+        private StorageContext DB { get; set; }
+        private HomeController Controller { get; set; }
+        public HomeControllerTests()
+        {
+            DB = new StorageContextMock().DB;
+            Controller = new HomeController(DB);
+        }
+        public void Dispose()
+        {
+            //Cleanup
+        }
         [Fact]
         public void IndexTest()
         {
-            //TODO: Implement
-            Assert.False(true);
+            IndexViewModel ViewModel;
+
+            //Test without characters redirects to Create
+            Assert.False(DB.Characters.Any());
+            var RedirectActionName = (Controller.Index() as RedirectToActionResult).ActionName;
+            Assert.Equal(@"Create", RedirectActionName);
+
+            //Add some characters
+            DB.Characters.Add(new Character()
+            {
+                Name = "hello"
+            });
+            DB.Characters.Add(new Character()
+            {
+                Name = "hi"
+            });
+            DB.SaveChanges();
+
+            //Test without Id specified
+            Assert.True(DB.Characters.Any());
+            ViewModel = (Controller.Index() as ViewResult).Model as IndexViewModel;
+            Assert.Equal(DB.Characters.First(), ViewModel.ActiveCharacter);
+            Assert.Equal(DB.Characters.Count(), ViewModel.CharacterList.Count());
+
+            //Test with Id specified
+            ViewModel = (Controller.Index(DB.Characters.Skip(1).First().Id) as ViewResult).Model as IndexViewModel;
+            Assert.Equal(DB.Characters.Skip(1).First(), ViewModel.ActiveCharacter);
+            Assert.Equal(DB.Characters.Count(), ViewModel.CharacterList.Count());
         }
         [Fact]
         public void CreateTest()
---
+++ test/CharacterCreator.Tests/Services/StorageContextMock.cs
@@ -0,0 +1,16 @@
+﻿using CharacterCreator.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharacterCreator.Tests.Services
+{
+    public class StorageContextMock
+    {
+        public StorageContext DB { get; set; }
+        public StorageContextMock()
+        {
+            DB = new StorageContext(new DbContextOptionsBuilder<StorageContext>()
+                .UseInMemoryDatabase()
+                .Options);
+        }
+    }
+}
